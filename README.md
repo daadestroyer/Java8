a3ab5b20267c8d1c7c551140249cc05c12a6ab5b
daadestroyer <nigamshubham2000@gmail.com>
2020-12-17 15:44:39 +0530

    Predicate Joining Completed...

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# Java 8 Concepts by Shubham Nigam (daadestroyer)
+# Java 8 Concepts by Shubham Nigam (daadestroyer) <a href="https://www.linkedin.com/in/daadestroyer/">LinkedIn</a>
 
-#### Functional Interface
-#### Lambda Expression
-#### Default Methods
-#### Static Methods
-#### Predefined Functional Interface
+#### 1. Functional Interface
+#### 2. Lambda Expression
+#### 3. Default Methods
+#### 4. Static Methods
+#### 5. Predefined Functional Interface
 &nbsp;&nbsp; a) Predicate <br> 
 &nbsp;&nbsp; b) Function <br>
 &nbsp;&nbsp; c) Consumer <br>
--- src/PredicatesJoining_05/App01_PredicatesJoining.java
+++ src/PredicatesJoining_05/App01_PredicatesJoining.java
@@ -1,7 +1,43 @@
 package PredicatesJoining_05;
 
+import java.util.function.Predicate;
+
 public class App01_PredicatesJoining {
+
+	static void m1(Predicate<Integer> p, int[] x) {
+		for (int i : x) {
+			if (p.test(i)) {
+				System.out.print(i + " ");
+			}
+		}
+	}
+
 	public static void main(String[] args) {
+
+		int[] x = { 0, 5, 10, 15, 20, 25, 30 };
+
+		Predicate<Integer> p1 = i -> i > 10;
+		Predicate<Integer> p2 = i -> i % 2 == 0;
+
+		System.out.println("Numbers greater than 10 are ");
+		m1(p1, x);
+		System.out.println("\n");
+
+		System.out.println("Even numbers are ");
+		m1(p2, x);
+		System.out.println("\n");
+
+		System.out.println("Numbers not greater than 10 are");
+		m1(p1.negate(), x);
+		System.out.println("\n");
+
+		System.out.println("Numbers greater than 10 or even are");
+		m1(p1.or(p2), x);
+		System.out.println("\n");
+
+		System.out.println("Numbers greater than 10 and even are");
+		m1(p1.and(p2), x);
+		System.out.println("\n");
 
 	}
 }
