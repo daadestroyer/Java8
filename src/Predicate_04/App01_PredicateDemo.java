ea2eec1898598de7133c141ae7ac88290b7bcce4
daadestroyer <nigamshubham2000@gmail.com>
2020-12-17 15:25:47 +0530

    README.md added

--- src/Predicate_04/App01_PredicateDemo.java
+++ src/Predicate_04/App01_PredicateDemo.java
@@ -1,4 +1,6 @@
 package Predicate_04;
+import java.util.ArrayList;
+import java.util.Collection;
 // Predicate is a single arguement function which perform conditional check and return boolean values as a result
 import java.util.function.Predicate;
 
@@ -7,12 +9,17 @@ public class App01_PredicateDemo {
 		Predicate<Integer> predicate = (i) -> i > 10; // Predicate to check no is greater than 10 or not
 		Predicate<String> predicate2 = (s) -> s.length() > 0; // Predicate to check length of string is greater then zero or not
 		Predicate<Integer> predicate3 = (i) -> i % 2 == 0;
-		
+		Predicate<Collection> predicate4 = (c)->c.isEmpty();
 		
 		System.out.println(predicate.test(40));
 		System.out.println(predicate2.test("Shubham"));
 		System.out.println(predicate3.test(5));
 	
+		ArrayList al = new ArrayList();
+		al.add(1);
+		al.add(2);
+		al.add(3);
+		System.out.println(predicate4.test(al));
 	
 	
 	
---
+++ src/PredicatesJoining_05/App01_PredicatesJoining.java
@@ -0,0 +1,7 @@
+package PredicatesJoining_05;
+
+public class App01_PredicatesJoining {
+	public static void main(String[] args) {
+
+	}
+}
