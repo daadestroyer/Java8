794be18ee8a6ab972642f80a9e24247245281ffc
daadestroyer <nigamshubham2000@gmail.com>
2020-12-17 11:17:39 +0530

    README.md deleted...

---
+++ src/LambdaExpression/App04_LamdaExpressionInMultithreading.java
@@ -0,0 +1,39 @@
+package LambdaExpression;
+
+/*
+// without lamda expression
+class MyRunnable implements Runnable {
+	public void run() {
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Child Thread...");
+		}
+	}
+}
+
+public class App04_LamdaExpressionInMultithreading {
+	public static void main(String[] args) {
+		Runnable r = new MyRunnable();
+		Thread t = new Thread(r);
+		t.start();
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Main Thread...");
+		}
+	}
+}
+*/
+public class App04_LamdaExpressionInMultithreading {
+	public static void main(String[] args) {
+		Runnable r = () -> {
+			for (int i = 0; i < 5; i++) {
+				System.out.println("Child Thread using Lamda Expression");
+			}
+		};
+
+		Thread t = new Thread(r);
+		t.start();
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Main Thread using Lamda Expression");
+		}
+	}
+}
---
+++ src/LambdaExpression/App05_LambdaExpressionWithAnonymousInnerClass.java
@@ -0,0 +1,33 @@
+package LambdaExpression;
+/*
+// Without lambda expression
+
+class MyRun implements Runnable {
+	public void run() {
+		System.out.println("Run without lambda expression");
+	}
+}
+
+public class App05_LambdaExpressionWithAnonymousInnerClass {
+	public static void main(String[] args) {
+		MyRun run = new MyRun();
+		Thread t = new Thread(run);
+		t.start();
+	}
+}
+*/
+
+// With Lamda Expression
+
+public class App05_LambdaExpressionWithAnonymousInnerClass {
+	public static void main(String[] args) {
+		Runnable r = new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("Run with lambda expression");
+			}
+		};
+		Thread t = new Thread(r);
+		t.start();
+	}
+}
