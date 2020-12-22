c0a9a469a7dbc3d5bb3c4b9788205758029f9505
daadestroyer <nigamshubham2000@gmail.com>
2020-12-22 22:26:29 +0530

    Demo program for supplier completed...

---
+++ src/Supplier_08/App01_DemoProgramForSupplier.java
@@ -0,0 +1,39 @@
+package Supplier_08;
+
+import java.util.function.Supplier;
+
+public class App01_DemoProgramForSupplier {
+	public static void main(String[] args) {
+		Supplier<String> S = () -> {
+			String[] s1 = { "Sunny", "Bunny", "Chinny", "Pinny" };
+			int x = (int) (Math.random() * 4);
+			return s1[x];
+		};
+		System.out.println(S.get());
+		System.out.println(S.get());
+		System.out.println(S.get());
+	}
+}
+/*
+Bunny
+Pinny
+Chinny
+
+Pinny
+Bunny
+Bunny
+
+Sunny
+Sunny
+Bunny
+
+Chinny
+Sunny
+Pinny
+
+Pinny
+Sunny
+Pinny
+
+
+*/
