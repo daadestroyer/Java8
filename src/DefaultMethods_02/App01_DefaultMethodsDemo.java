b4ed9842256f0585acebe726cf59390b903f75fc
Shubham Nigam <apple@Apples-MacBook-Pro.local>
2023-04-07 16:33:11 +0530

    static and default method notes added...

--- src/DefaultMethods_02/App01_DefaultMethodsDemo.java
+++ src/DefaultMethods_02/App01_DefaultMethodsDemo.java
@@ -1,5 +1,11 @@
 package DefaultMethods_02;
 
+/*
+1. if a new method added in interface before java 7 all classes forcefully need to implement that method, to overcome
+   this issue in java8 default method is introduced which has body
+
+2. we can use default method in child classes , means default method support inheritance
+ */
 interface Left {
 	default void m1() {
 		System.out.println("m1 default method");
--- src/StaticMethods_03/App01_StaticMethodsDemo.java
+++ src/StaticMethods_03/App01_StaticMethodsDemo.java
@@ -1,18 +1,24 @@
 package StaticMethods_03;
 
-interface Interf {
-	static void m1() {
-		System.out.println("Interface Static method");
-	}
+/*
+1. The interface static methods are similar to static methods of class
 
-	default void m2() {
-		System.out.println("Interface Default Method");
-	}
+2. overriding of interface staic method is not allowed
+
+3. child classes can use interface static method just by using interface reference name only
+*/
+interface Interf {
+    static void m1() {
+        System.out.println("Interface Static method");
+    }
+
+    default void m2() {
+        System.out.println("Interface Default Method");
+    }
 }
 
 public class App01_StaticMethodsDemo implements Interf {
-	public static void main(String[] args) {
-		Interf.m1(); // we can call interface static method by interface name only
-
-	}
+    public static void main(String[] args) {
+        Interf.m1(); // we can call interface static method by interface name only
+    }
 }
